ac6ef3d58254fd55b20fcb0679da5fb03dcbc24e
j-after-hours <jakub.b.j92@gmail.com>
2024-09-17 00:30:40 +0200

    Added player drawing

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 import sys
 from constants import *
+from player import Player
 
 
 def handle_close_btn():
@@ -13,13 +14,15 @@ def handle_close_btn():
 
 def play():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     clock = pygame.time.Clock()
     dt = 0
 
     while True:
         handle_close_btn()
 
-        pygame.Surface.fill(screen, (0, 0, 0))
+        pygame.Surface.fill(screen, "black")
+        player.draw(screen)
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
