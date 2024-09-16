b76b5f5e937e94e42c43fae428d8b23053d17485
j-after-hours <jakub.b.j92@gmail.com>
2024-09-16 23:31:27 +0200

    Game loop addded

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
 import pygame
+import sys
 from constants import *
 
 
+def handle_close_btn():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            print("X button pressed - exiting the game")
+            pygame.quit()
+            sys.exit()
+
+
+def play():
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        handle_close_btn()
+        pygame.Surface.fill(screen, (0, 0, 0))
+        pygame.display.flip()
+
+
 def main():
+    pygame.init()
     print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    play()
 
 
 if __name__ == "__main__":
